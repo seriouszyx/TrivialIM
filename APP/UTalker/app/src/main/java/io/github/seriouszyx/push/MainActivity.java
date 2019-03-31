f0beaaba2812ff5513e4a3ceb3b1d3bced3082d4
seriouszyx <674965440@qq.com>
2019-03-31 17:25:53 +0800

    修复基类Activity的注解错误

---
+++ APP/UTalker/app/src/main/java/io/github/seriouszyx/push/MainActivity.java
@@ -0,0 +1,27 @@
+package io.github.seriouszyx.push;
+
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+
+    @BindView(R.id.txt_test)
+    TextView mTestText;
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+        mTestText.setText("Test Hello");
+    }
+}
---
+++ APP/UTalker/common/src/main/java/io/github/seriouszyx/common/test.java
@@ -0,0 +1,4 @@
+package io.github.seriouszyx.common;
+
+public class test {
+}
