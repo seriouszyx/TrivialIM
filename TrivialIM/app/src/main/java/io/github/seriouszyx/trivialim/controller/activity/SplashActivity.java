c68b971e147c10ddcc8cb0c687cc691f4e7bd875
seriouszyx <674965440@qq.com>
2019-05-11 10:53:19 +0800

    完成欢迎页面

--- TrivialIM/app/src/main/java/io/github/seriouszyx/trivialim/controller/activity/SplashActivity.java
+++ TrivialIM/app/src/main/java/io/github/seriouszyx/trivialim/controller/activity/SplashActivity.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 
 import io.github.seriouszyx.trivialim.R;
 
-public class MainActivity extends Activity {
+// 欢迎页面
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
