8eb68aaee0f272289e71f434b5cf664b9184ba96
seriouszyx <674965440@qq.com>
2019-05-12 13:19:09 +0800

    完成联系人列表页面

--- TrivialIM/app/src/main/java/io/github/seriouszyx/trivialim/controller/fragment/ContactListFragment.java
+++ TrivialIM/app/src/main/java/io/github/seriouszyx/trivialim/controller/fragment/ContactListFragment.java
@@ -1,6 +1,24 @@
 package io.github.seriouszyx.trivialim.controller.fragment;
 
 import android.support.v4.app.Fragment;
+import android.view.View;
 
-public class ContactListFragment extends Fragment {
+import com.hyphenate.easeui.ui.EaseContactListFragment;
+
+import io.github.seriouszyx.trivialim.R;
+
+// 联系人列表页面
+public class ContactListFragment extends EaseContactListFragment {
+
+    @Override
+    protected void initView() {
+        super.initView();
+        // 布局显示加号
+        titleBar.setRightImageResource(R.drawable.em_add);
+
+        // 头布局添加
+        View headerView = View.inflate(getActivity(), R.layout.header_fragment_contact, null);
+
+        listView.addHeaderView(headerView);
+    }
 }
