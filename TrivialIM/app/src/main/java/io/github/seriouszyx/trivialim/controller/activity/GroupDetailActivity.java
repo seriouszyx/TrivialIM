c5a14b5db179014974f3722d3f6dcd8f65b217b3
seriouszyx <674965440@qq.com>
2019-06-12 16:33:18 +0800

    添加资源文件，完成群信息处理

--- TrivialIM/app/src/main/java/io/github/seriouszyx/trivialim/controller/activity/GroupDetailActivity.java
+++ TrivialIM/app/src/main/java/io/github/seriouszyx/trivialim/controller/activity/GroupDetailActivity.java
@@ -5,27 +5,42 @@ import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
 import com.hyphenate.exceptions.HyphenateException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.github.seriouszyx.trivialim.R;
+import io.github.seriouszyx.trivialim.controller.adapter.GroupDetailAdapter;
 import io.github.seriouszyx.trivialim.model.Model;
+import io.github.seriouszyx.trivialim.model.bean.UserInfo;
 import io.github.seriouszyx.trivialim.utils.Constant;
 
 /**
  * 群详情页面
  */
+
+//群详情页面
 public class GroupDetailActivity extends Activity {
 
     private GridView gv_groupdetail;
     private Button bt_groupdetail_out;
+    private TextView tv_groupdetail_groupname;
+    private TextView tv_groupdetail_describe;
+    private Toolbar toolbar;
     private EMGroup mGroup;
+    private List<UserInfo> mUsers;
+    private GroupDetailAdapter groupDetailAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +48,62 @@ public class GroupDetailActivity extends Activity {
         setContentView(R.layout.activity_group_detail);
 
         initView();
-
         getData();
-
         initData();
+        initListener();
+    }
+
+    private void initView() {
+        gv_groupdetail = findViewById(R.id.gv_groupdetail);
+        bt_groupdetail_out = findViewById(R.id.bt_groupdetail_out);
+        tv_groupdetail_groupname = findViewById( R.id.tv_groupdetail_groupname );
+        tv_groupdetail_describe = findViewById( R.id.tv_groupdetail_describe );
+        toolbar = findViewById( R.id.toolbar );
+    }
+
+    //获取传递过来的数据
+    private void getData(){
+        Intent intent = getIntent();
+        String groupId = intent.getStringExtra(Constant.GROUP_ID);
+        if(groupId == null){
+            return;
+        }else {
+            mGroup = EMClient.getInstance().groupManager().getGroup(groupId);
+        }
     }
 
     private void initData() {
-        // 初始化button显示
+
+        toolbar.setNavigationOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        } );
+        //初始化button显示
         initButtonDisplay();
+
+        //初始化群名称
+        initGroupInfo();
+
+        //初始化gridview
+        initGridview();
+
+        //从环信服务器获取所有群成员
+        getMembersFromHxServer();
     }
 
+    private void initGroupInfo() {
+        String groupName = mGroup.groupSubject();
+        String groupDescribe = mGroup.getDescription();
+        tv_groupdetail_groupname.setText( groupName );
+        tv_groupdetail_describe.setText( groupDescribe );
+    }
+
+
     private void initButtonDisplay() {
-        if (EMClient.getInstance().getCurrentUser().equals(mGroup.getOwner())) {
-            // 群主
+        //判断当前是否是群主
+        if(EMClient.getInstance().getCurrentUser().equals(mGroup.getOwner())){//群主
             bt_groupdetail_out.setText("解散群");
             bt_groupdetail_out.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -55,26 +112,28 @@ public class GroupDetailActivity extends Activity {
                         @Override
                         public void run() {
                             try {
-                                // 去环信服务器解散群
+                                //去环信服务器解散群
                                 EMClient.getInstance().groupManager().destroyGroup(mGroup.getGroupId());
-                                // 发送退群的广播
-                                exitGroupBroatCast();
-                                
+
+                                //发送群解散广播
+                                exitGroupBroadCast();
+
+                                //更新页面
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        Toast.makeText(GroupDetailActivity.this, "解散群成功", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(GroupDetailActivity.this,"解散群成功",Toast.LENGTH_SHORT).show();
+
+                                        //结束当前页面
                                         finish();
                                     }
                                 });
-
                             } catch (final HyphenateException e) {
                                 e.printStackTrace();
-                                
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        Toast.makeText(GroupDetailActivity.this, "集散群失败" + e.toString(), Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(GroupDetailActivity.this,"解散群失败"+e.toString(),Toast.LENGTH_SHORT).show();
                                     }
                                 });
                             }
@@ -82,8 +141,7 @@ public class GroupDetailActivity extends Activity {
                     });
                 }
             });
-        } else {
-            // 群成员
+        }else{//群成员
             bt_groupdetail_out.setText("退群");
 
             bt_groupdetail_out.setOnClickListener(new View.OnClickListener() {
@@ -93,25 +151,26 @@ public class GroupDetailActivity extends Activity {
                         @Override
                         public void run() {
                             try {
-                                // 告诉环信服务器退群
+                                //去环信服务器退群
                                 EMClient.getInstance().groupManager().leaveGroup(mGroup.getGroupId());
-                                 exitGroupBroatCast();
-                                 
-                                 runOnUiThread(new Runnable() {
-                                     @Override
-                                     public void run() {
-                                         Toast.makeText(GroupDetailActivity.this, "退群成功", Toast.LENGTH_SHORT).show();
 
-                                         finish();
-                                     }
-                                 });
-                            } catch (HyphenateException e) {
-                                e.printStackTrace();
-                                
+                                //发送退群广播
+                                exitGroupBroadCast();
+
+                                //更新页面
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        Toast.makeText(GroupDetailActivity.this, "退群失败", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(GroupDetailActivity.this,"退群成功" ,Toast.LENGTH_SHORT).show();
+                                        finish();
+                                    }
+                                });
+                            } catch (final HyphenateException e) {
+                                e.printStackTrace();
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(GroupDetailActivity.this,"退群失败"+e.toString() ,Toast.LENGTH_SHORT).show();
                                     }
                                 });
                             }
@@ -122,29 +181,166 @@ public class GroupDetailActivity extends Activity {
         }
     }
 
-    // 发送退群和解散群广播
-    private void exitGroupBroatCast()
-    {
-        LocalBroadcastManager mLBM = LocalBroadcastManager.getInstance(GroupDetailActivity.this);
-        Intent intent = new Intent(Constant.EXIT_GROUP);
-        intent.putExtra(Constant.GROUP_ID, mGroup.getGroupId());
-        mLBM.sendBroadcast(intent);
+    private void initGridview() {
+        //当前用户是群主或者群公开了
+        boolean isCanModify = EMClient.getInstance().getCurrentUser().equals(mGroup.getOwner()) || mGroup.isPublic();
+
+        groupDetailAdapter = new GroupDetailAdapter(this,isCanModify,mOnGroupDetailListener);
+        gv_groupdetail.setAdapter(groupDetailAdapter);
     }
 
-    // 获取传递过来的数据
-    private void getData() {
-        Intent intent = getIntent();
-        String groupId = intent.getStringExtra(Constant.GROUP_ID);
-        if (groupId == null) {
-            return;
-        } else {
-            mGroup = EMClient.getInstance().groupManager().getGroup(groupId);
+    private void getMembersFromHxServer() {
+        Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //从环信服务器获取所有群成员信息
+                    EMGroup emGroup = EMClient.getInstance().groupManager().getGroupFromServer(mGroup.getGroupId());
+                    List<String> members = emGroup.getMembers();
+
+                    if(members != null && members.size() >= 0){
+                        mUsers = new ArrayList<>();
+
+                        //转换
+                        for(String member : members){
+                            UserInfo userInfo = new UserInfo(member);
+                            mUsers.add(userInfo);
+                        }
+
+                        //刷新页面
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //刷新适配器
+                                groupDetailAdapter.refresh(mUsers);
+                            }
+                        });
+                    }
+                } catch (final HyphenateException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(GroupDetailActivity.this,"获取群信息失败"+e.toString(),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    private void initListener() {
+        gv_groupdetail.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        //判断当前是否是删除模式
+                        if(groupDetailAdapter.ismIsDeleteModel()){
+                            //切换为非删除模式
+                            groupDetailAdapter.setmIsDeleteModel(false);
+
+                            //刷新页面
+                            groupDetailAdapter.notifyDataSetChanged();
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private GroupDetailAdapter.OnGroupDetailListener mOnGroupDetailListener = new GroupDetailAdapter.OnGroupDetailListener() {
+        //添加群成员
+        @Override
+        public void onAddMembers() {
+            //跳转到选择联系人页面
+            Intent intent = new Intent(GroupDetailActivity.this, PickContactActivity.class);
+
+            //传递群id
+            intent.putExtra(Constant.GROUP_ID,mGroup.getGroupId());
+            startActivityForResult(intent,2);
         }
 
+        //删除群成员方法
+        @Override
+        public void onDeleteMembers(final UserInfo user) {
+            Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //从环信服务器删除此人
+                        EMClient.getInstance().groupManager().removeUserFromGroup(mGroup.getGroupId(),user.getHxid());
+                        //更新页面
+                        getMembersFromHxServer();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(GroupDetailActivity.this,"删除"+user.getName()+"成功",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } catch (final HyphenateException e) {
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(GroupDetailActivity.this,"删除失败"+e.toString(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            //获取返回的准备邀请的群成员信息
+            final String[] memberses = data.getStringArrayExtra("members");
+
+            Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //去环信服务器，发送邀请信息
+                        EMClient.getInstance().groupManager().addUsersToGroup(mGroup.getGroupId(),memberses);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(GroupDetailActivity.this,"发送邀请成功",Toast.LENGTH_SHORT).show();
+                                groupDetailAdapter.refresh(mUsers);
+
+                            }
+                        });
+                    } catch (final HyphenateException e) {
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(GroupDetailActivity.this,"发送邀请失败"+e.toString(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 
-    private void initView() {
-        gv_groupdetail = findViewById(R.id.gv_groupdetail);
-        bt_groupdetail_out = findViewById(R.id.bt_groupdetail_out);
+
+    //发送退群和解散广播群
+    private void exitGroupBroadCast(){
+        LocalBroadcastManager mLBM = LocalBroadcastManager.getInstance(GroupDetailActivity.this);
+        Intent intent = new Intent(Constant.EXIT_GROUP);
+
+        intent.putExtra(Constant.GROUP_ID,mGroup.getGroupId());
+        mLBM.sendBroadcast(intent);
+
     }
+
 }
